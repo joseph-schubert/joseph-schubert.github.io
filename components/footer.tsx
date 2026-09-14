d3ea6059ec74d793810392b1955413803730d5ec
joseph-schubert <josephschubert3@gmail.com>
2026-09-14 10:18:36 +1000

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,10 +3,10 @@ export function Footer() {
     <footer className="py-12 px-6 border-t border-neutral-100">
       <div className="max-w-4xl mx-auto text-center">
         <a
-          href="mailto:joseph.schubert@qm.qld.gov.au"
+          href="mailto:josephschubert3@yahoo.com"
           className="text-neutral-600 hover:text-neutral-900 transition-colors"
         >
-          joseph.schubert@qm.qld.gov.au
+          josephschubert3@yahoo.com
         </a>
         <p className="text-neutral-400 text-sm mt-4">
           &copy; 2026 Joseph Schubert.
